f3cd42d3a6755c28e147213ed22082867416ca34
Your NameIvanKushnyr <you@example.com>
2019-02-16 16:09:03 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,72 @@
+var arr = [2,3,4,-1,5,234,-24] , sun = 0
+for (var i = 0; i < arr.length; i++) {
+ if(arr[i] > 0){
+ 	sum = sum+arr [i]
+ }
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var arr = [1,5,-7,-10,5,4,32,-2,-84,446] , count = 0 ;
+// for (var i = 0; i < arr.length; i++) {
+// 	if (arr[i] > 0) {
+// 		count ++ ;
+// console.log(arr[i])
+// }
+//  }
+// console.log(count) ;
+
+// var arr =[ 
+// [1,2,3], //0
+// [ "q","qwe" , "elem"] //1
+// [true , 2223 "some text"] //2
+// ] ;
+// console.log(arr[2][1][1])
+// for (var i = 0; i < arr.length; i++) {
+// 	// console.log(arr[i])
+// for (var j = 0; j < arr[i].length; j++) {
+// 	console.log(arr[i][j])
+// }
+// }
+
+// var arr = ["Cat1", "Cat2" ," Cat3"] ;
+// console.log(arr[1])
+// arr[1] = "Some element" ;
+// console.log(arr[1])
+// console.log(arr)
+// arr[3] = "New Element" ;
+// console.log(arr);
+// console.log(arr.lenght);
+// console.log(arr[arr.lenght]-1) ;
+// console.log (arr.pop()); //удаляет последний елемент массива
+// console.log(arr)
+// arr.push("Новий елемент" , 25 , true , "last element")// добавляет в конце массива
+// console.log(arr)
+// arr.shift()//удаляет елемент массива
+// arr.unshift("добавляет новий елемент в массиве");
+// console.log(arr)
+// for (var i = 0; i < arr.length; i++) {
+// 	console.log(arr[i]) ;
+// }
